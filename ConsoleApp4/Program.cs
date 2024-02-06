e1b1f2e3469a5a4c5927b6128526f99e11e18422
Claire Lee <jclairelee@gmail.com>
2024-02-06 13:36:26 -0800

    method

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab_wk05
+{
+   internal class Program
+{
+    static double calculate(double inputA, double inputB, double inputC)
+    {
+        double result = (inputA+inputB+inputC)/3;
+        return result;
+    }
+    static void Main(string[] args)
+    {
+        int num1 = 50;
+        int num2 = 20;
+        int num3 = 40;
+        double average = calculate(num1, num2, num3);
+        Console.WriteLine($"the average is {average:F1}");
+    }
+}
+}
