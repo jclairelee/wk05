993e13975aa8f46a02a96ad5eaadf8de2160a5ba
Claire Lee <jclairelee@gmail.com>
2024-02-06 13:04:25 -0800

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab_wk05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 20;
+            int counter = 0;
+            
+            while (counter <= N)
+            {
+                Console.WriteLine(counter);
+                counter += 2;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lab_wk05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+         // Generate a random number between 0 and 100
+
+        int randomNumber = new Random().Next(0, 100);
+
+        int guessCount = 0;
+
+        while(true){
+            // Ask user to enter an input
+            Console.WriteLine("Guess a number between 0 and 100");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            guessCount++;
+
+            if(guess  == randomNumber)
+            {
+                Console.Write($"Congrates! you one!");
+                Console.Write($"It took you {guessCount} guesses");
+                break;
+              
+            } else if (guess < randomNumber){
+                Console.WriteLine("⬆ ⬆ ⬆ UP ⬆ ⬆ ⬆");
+            } else {
+                Console.WriteLine("⬇ ⬇ ⬇ DOWN ⬇ ⬇ ⬇");
+            }
+        }
+        }
+    }
+}
